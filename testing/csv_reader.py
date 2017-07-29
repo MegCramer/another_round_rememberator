e6909cb0ddd1e5cb5919df2e8e746394f4d11787
Meg Cramer <megcramer@MegCramer-MBPr.home>
2017-07-29 15:58:52 -0400

    for testing and troubleshooting

---
+++ testing/csv_reader.py
@@ -0,0 +1,35 @@
+import csv
+
+# loading data from csv
+
+def share_url():
+    ep_num = []
+    ep_title = []
+    ep_air_date = []
+    ep_guest1 = []
+    ep_round_t = []
+    ep_round_h = []
+    ep_url = []
+
+    with open('data.csv', 'rU') as f:
+        csvReader = csv.reader(f)
+        for row in csvReader:
+
+            ep_num.append(row[0])
+            ep_title.append(row[1])
+            ep_air_date.append(row[2])
+            ep_guest1.append(row[3])
+            ep_round_t.append(row[4])
+            ep_round_h.append(row[5])
+            ep_url.append(row[6])
+
+            print(ep_title)
+            print(ep_air_date)
+            print(ep_guest1)
+            print(ep_round_t)
+            print(ep_round_h)
+            print(ep_url)
+            print('------')
+
+
+share_url()
