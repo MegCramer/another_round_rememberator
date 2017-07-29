fa047ff6d5dfbc3d57c755fb8905341acc9b6470
Meg Cramer <megcramer@MegCramer-MBPr.home>
2017-07-29 17:42:26 -0400

    updated to include a failure msg

--- csv_dict.py
+++ csv_dict.py
@@ -3,18 +3,23 @@ import sys
 
 # loading data from csv
 
-def share_url(search_input):
-
+def find_episode(search_input):
     with open('data.csv', 'rU') as f:
         csvReader = csv.DictReader(f)
         for episode in csvReader:
             if episode['ep_guest1'] == search_input:
                 return episode
-            if episode['ep_round_t'] == search_input:
+            elif episode['ep_round_t'] == search_input:
                 return episode
-            if episode['ep_round_h'] == search_input:
+            elif episode['ep_round_h'] == search_input:
                 return episode
 
+        return None
+
+
 if __name__ == '__main__':
-    episode_match = share_url(sys.argv[1])
-    print('Here is {}. Is this the episode you were looking for? {}'.format(episode_match['ep_title'], episode_match['ep_url']))
+    episode_match = find_episode(sys.argv[1])
+    if episode_match == None:
+        print('I couldn\'t find that episode. Did you check your spelling?')
+    else:
+        print('Here is {}. Is this the episode you were looking for? (Y/N) {}'.format(episode_match['ep_title'], episode_match['ep_url']))
